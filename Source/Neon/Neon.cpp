311f83aa1c169e56e24e7af2fd8276a250920477
R4WRity <requiem4ddr@gmail.com>
2022-09-25 20:31:23 -0700

    We're Back!
    Game is buildable and playable front to back.
    TO DO:
    - Add Quit Sequence to endgame
    -Set (windowed) Maximum Resolution for game launch

---
+++ Source/Neon/Neon.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Neon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Neon, "Neon" );
